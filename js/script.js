ddbc6ede4e78f1d148cc163a76b797fa89aab5ac
JiHyun <137370463+HwangJihyunnn@users.noreply.github.com>
2023-10-23 14:58:19 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ $(function() {
         $('.gnb .sub').fadeOut(100)
     })
 });
+
+$(function(){
+    $('.mobile-btn').click(()=>{
+        $('.mobile-gnb, .mobile-bg').stop().fadeIn(300)
+    });
+    $(".gnb-close").click(()=>{
+    $('.mobile-gnb, .mobile-bg').stop().fadeOut(300)
+    })
+})
